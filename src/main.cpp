e654e29637c67706275e0362790e70ce4d82c3f2
syd kahn <sydkahn@sydkahn.com>
2022-03-27 14:51:16 -0400

    after publish

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
   Github & WiKi: https://github.com/ayushsharma82/ESPConnect
   Works with both ESP8266 & ESP32
 */
-#define MQTT_CLIENTID "esp32-mqtt-deepsleep"
+#define DEBUG_MSG
 #define MQTT_HOST "192.168.0.23"
 #define MQTT_PORT 1883
 
@@ -22,11 +22,15 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+const String MQTT_OUTTOPIC = "esp8266";
+const String MQTT_CLIENTID = "esp8266-mqtt";
 #elif defined(ESP32)
 /* ESP32 Dependencies */
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+const String MQTT_OUTTOPIC = "esp32";
+const String MQTT_CLIENTID = "esp32-mqtt";
 #endif
 
 #include <ESPConnect.h>
@@ -37,14 +41,15 @@ WiFiClient wifiClient;
 
 PubSubClient client(wifiClient);
 time_t startTime = millis();
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
 struct weatherData
 {
   float temperature;
   float pressure;
   float humidity;
 };
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
 
 AsyncWebServer server(80);
 
@@ -67,6 +72,20 @@ struct weatherData readSensor(uint8_t bmeAddress = 0x76)
   return measurement;
 }
 
+/// Convert weatherDataStruct to jason object
+String Convert2Jason(struct weatherData weather)
+{
+  String jsn = "{\"temperature\",";
+  jsn += String(weather.temperature);
+  jsn += ",\"pressure\",";
+  jsn += String(weather.pressure);
+  jsn += ",\"humidity\",";
+  jsn += String(weather.humidity);
+  jsn += "}";
+  return jsn;
+}
+
+/// Print data in the weather structure
 void printValues(struct weatherData dataToPrint)
 {
   Serial.print("Temperature = ");
@@ -81,6 +100,66 @@ void printValues(struct weatherData dataToPrint)
   Serial.println(" %");
 
   Serial.println();
+}
+
+#ifdef esp32
+const MQTT_OUTTOPIC "esp32";
+const MQTT_CLIENTID "esp32-mqtt";
+#endif
+#ifdef ESP8266
+const MQTT_OUTTOPIC "esp8266";
+const MQTT_CLIENTID "esp8266-mqtt";
+#endif
+
+void publishJson(String value, String subTopic)
+{
+  /*
+  this takes a float measurement value - and builds a topic string
+  then publishes the floating value to the topc
+  */
+#ifdef DEBUG_MSG
+  Serial.print(subTopic);
+  Serial.print(": ");
+  Serial.println(value);
+
+#endif
+  String topic = MQTT_OUTTOPIC;
+  topic = MQTT_OUTTOPIC;
+  topic.concat("/");
+  topic.concat(subTopic);
+  // topic.concat("/");
+  char topic_buf[topic.length() + 1];
+  topic.toCharArray(topic_buf, topic.length() + 1);
+#ifdef DEBUG_MSG
+  Serial.println(topic_buf);
+#endif
+
+  client.publish(topic_buf, value.c_str(), true);
+}
+void publishMeasurement(float value, String subTopic)
+{
+  /*
+  this takes a float measurement value - and builds a topic string
+  then publishes the floating value to the topic
+  */
+#ifdef DEBUG_MSG
+  Serial.print(subTopic);
+  Serial.print(": ");
+  Serial.println(value);
+
+#endif
+  String topic = MQTT_OUTTOPIC;
+  topic = MQTT_OUTTOPIC;
+  topic.concat("/");
+  topic.concat(subTopic);
+  // topic.concat("/");
+  char topic_buf[topic.length() + 1];
+  topic.toCharArray(topic_buf, topic.length() + 1);
+#ifdef DEBUG_MSG
+  Serial.println(topic_buf);
+#endif
+
+  client.publish(topic_buf, String(value).c_str(), true);
 }
 void setup()
 {
@@ -114,6 +193,7 @@ void loop()
 {
   Serial.println("Start loop()");
   // Loop until we're reconnected
+  String clientId = MQTT_CLIENTID + WiFi.macAddress();
 
   while (!mqttClient.connected())
   {
@@ -127,18 +207,37 @@ void loop()
       Serial.println("WiFi not Connected");
     }
 
-    
-
     mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-    if (mqttClient.connect(MQTT_CLIENTID))//, MQTT_USER, MQTT_PASSWORD))
+    if (mqttClient.connect(clientId.c_str())) //, MQTT_USER, MQTT_PASSWORD))
     {
       Serial.println("MQTT connected.");
     }
-    else{
+    else
+    {
       Serial.println("MQTT not connected.");
-
     }
     delay(1000);
   }
+  if (mqttClient.connected())
+  {
+    Serial.print("mqttClient IS connected as: ");
+    Serial.println(clientId);
+  }
+  else
+  {
+    Serial.println("mqttClient NOT connected");
+  }
+ 
+  weatherData WD = readSensor();
+  printValues(WD);
+  publishMeasurement(WD.temperature, "temperature");
+  delay(500);
+  publishMeasurement(WD.pressure, "pressure");
+  delay(500);
+  publishMeasurement(WD.humidity, "humidity");
+  String jsonString=Convert2Jason(WD);
+  Serial.println(jsonString);
+  publishJson(jsonString,"BEM280");
+  delay(500);
   delay(5000);
 }
